2d7de90d5cf0888bd8aaa4641fac348a86857c63
nobu <noburad1121@gmail.com>
2024-03-13 11:02:51 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang=ja>
+<?php
+
+$Path = "";
+        define("title" ,"トップ");
+        include('template/head.php');
+    ?>
+<link rel="canonical" href="">
+<meta name="description" content="">
+<meta name="keywords" content="">
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php include('template/header.php'); ?>
+        <main>
+            <section class="fv">
+                <video src="images/fv.mp4" autoplay loop muted></video>
+            </section>
+        </main>
+        <?php include('template/footer.php'); ?>
+    </div><!-- wrapper -->
+</body>
+<?php include('template/script.php'); ?>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,3 @@
+<header class="header-l_wrap">
+    <h1><img src="images/logo.png" alt=""></h1>
+</header>
